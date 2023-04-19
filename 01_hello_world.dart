1bb9b7ecacb3c3034655bff45293870d1310053f
AngelJostar <angelrojas@ciencias.unam.mx>
2023-04-19 00:43:05 -0600

    hola mundo}

---
+++ 01_hello_world.dart
@@ -0,0 +1,13 @@
+void main(){
+  
+  //var myName = 'Luis Angel';
+  //late final myName;
+  //const myName = 'Luis Angel';
+  String myName = 'Luis Angel';
+  
+  
+  print('hola $myName');
+  print('Hola ${myName.toUpperCase()}');
+  print('Hola ${1+1}');
+  
+}
