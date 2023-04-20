531356feb9febd54a9ae33b9074ae3bee681172f
AngelJostar <angelrojas@ciencias.unam.mx>
2023-04-20 16:58:05 -0600

    map

---
+++ 03_map.dart
@@ -0,0 +1,18 @@
+void main () {
+  
+  final pokemon = {
+    'name': 'Ditto',
+    'hp': '100',
+    'isAlive':true,
+    'abilities':<String>['impostor'],
+    'sprites': <int, String>{
+      1:'ditto/front.png',
+      2:'ditto/back.png'
+    }
+  };
+  
+  print(pokemon);
+  
+  print('Name: ${pokemon['name']}');
+  print('sprites')
+}
